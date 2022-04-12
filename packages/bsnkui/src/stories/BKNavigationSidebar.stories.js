e2a1ca27cd986ab94f695c73d8b7ed25d83c677a
bsnk-dev <61131051+bsnk-dev@users.noreply.github.com>
2022-04-12 18:56:06 -0500

    navigation sidebar adjustments

--- packages/bsnkui/src/stories/BKNavigationSidebar.stories.js
+++ packages/bsnkui/src/stories/BKNavigationSidebar.stories.js
@@ -1,4 +1,4 @@
-import { BkNavigationItem, BkNavigationSidebar, BkTitle } from '../components'
+import { BkNavigationItem, BkNavigationSidebar, BkTitle, BkSeparator } from '../components'
 
 export default {
   title: 'bsnkui/BKNavigationSidebar',
@@ -8,14 +8,15 @@ export default {
 }
 
 const Template = (args) => ({
-  components: { BkNavigationSidebar, BkNavigationItem, BkTitle },
+  components: { BkNavigationSidebar, BkNavigationItem, BkTitle, BkSeparator },
   setup () {
     return { args }
   },
   template: `
       <bk-navigation-sidebar v-bind="args">
         <bk-title>App Navigation</bk-title>
-        <bk-navigation-item>
+        <div class="nav_label text-color-primary_light">Services</div>
+        <bk-navigation-item v-for="i of 4" :key="i">
           <div style="display: flex; align-items: center">
             <svg xmlns="http://www.w3.org/2000/svg" style="height: 24px; width: 24px; margin-right: 5px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
@@ -24,7 +25,9 @@ const Template = (args) => ({
             <div>Hello World</div>
           </div>
         </bk-navigation-item>
-        <bk-navigation-item>
+        <div class="nav_label text-color-primary_light">Settings</div>
+        <bk-separator/>
+        <bk-navigation-item v-for="i of 3" :key="i">
           <div style="display: flex; align-items: center">
             <svg xmlns="http://www.w3.org/2000/svg" style="height: 24px; width: 24px; margin-right: 5px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
