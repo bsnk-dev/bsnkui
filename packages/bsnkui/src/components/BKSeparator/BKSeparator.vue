696b3dee8087af9286f87490986fa0b0f2d8c37a
bsnk-dev <61131051+bsnk-dev@users.noreply.github.com>
2022-04-12 18:57:02 -0500

    horizontal separator

--- packages/bsnkui/src/components/BKSeparator/BKSeparator.vue
+++ packages/bsnkui/src/components/BKSeparator/BKSeparator.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="bk_vertical_separator"></div>
+  <div :class="(horizontal) ? 'bk_horizontal_separator' : 'bk_vertical_separator'"></div>
 </template>
 
 <script>
 import './BKSeparator.scss'
 export default {
-  name: 'bkSeparator'
+  name: 'bkSeparator',
+
+  props: {
+    horizontal: {
+      type: Boolean,
+      default: true
+    }
+  }
 }
 </script>
